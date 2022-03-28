1c21331279a15f0925fb137fce8167a2681ee079
cil04105 <hojin6417@gmail.com>
2022-03-28 23:58:02 +0900

    Add: Bootstrap -Navbar, Jumbotrone

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,14 @@
+function Jumbotron(props) {
+  return (
+    <div className="jumboTrone">
+      <h1>Hello world</h1>
+      <p>
+        ehdgoanfhrk qorentksdl akfmrh ekfgehfhr gksmsladl qhdngktk dnflskfkakstp
+      </p>
+      <hr />
+      <button></button>
+    </div>
+  );
+}
+
+export default Jumbotron;
